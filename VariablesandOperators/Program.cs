5a9edeff3655fa40d8fa9db1a18cf4588302e216
Jerome <jscar216@gmail.com>
2017-06-27 15:05:42 -0400

    work

--- VariablesandOperators/Program.cs
+++ VariablesandOperators/Program.cs
@@ -10,6 +10,46 @@ namespace VariablesandOperators
     {
         static void Main(string[] args)
         {
+            //initializing new firstName variable
+            string firstName = "Jerome";
+            //Printing the value of firstName to the console
+            Console.WriteLine(firstName);
+            //intializing new birthMonth variable
+            string birthMonth = "January";
+            //Printing the value of birthMonth to the console
+            Console.WriteLine(birthMonth);
+            int age = 65;
+            string why = "because I said so";
+            char firstInitial = 'J';
+            //floating point types
+            float floatNumber = 2.164403304897294849383961032f;
+            Console.WriteLine(floatNumber);
+            double doubleNumber = 2.164403304897294849383961032d;
+            Console.WriteLine(doubleNumber);
+            decimal decimalNumber = 2.164403304897294849383961032m;
+            Console.WriteLine(decimalNumber);
+            int jessicaAge = 23;
+            int samAge = 47;
+            Console.WriteLine(jessicaAge < samAge);
+            Console.WriteLine("What is the name on the reservation");
+            string reserveName = Console.ReadLine();
+            Console.WriteLine("How many people are in your party");
+            int numberOfPeople = int.Parse(Console.ReadLine());
+            double amountEach = 13.53d;
+            double totalBill = numberOfPeople * amountEach;
+            Console.WriteLine("Reservation: " + reserveName);
+            Console.WriteLine("Total Due: $" + totalBill);
+
+            double cupsNeeded = 7;
+            Console.WriteLine("How many cups added");
+            double cupsAdded = double.Parse(Console.ReadLine());
+            double cupsLeft = cupsNeeded - cupsAdded;
+            Console.WriteLine("Add " + cupsLeft + " cups");
+
+
+                 
+
+
         }
     }
 }
